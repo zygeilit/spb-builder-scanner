1cfae0431bf32f245bd42e85e0998f6dbe878656
zhangyue <zhangyue@beisen.com>
2018-01-23 14:26:54 +0800

    tab cmp

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@
 var acorn = require('acorn')
 var objass = require('object-assign')
 
-var fnSignatureGetter = require('./sca-fn-signature-getter')
-var propertiesGetter = require('./sca-properties-getter')
+// 不同维度的信息提取的实现，对应到某一个文件
+var fnSignatureGetter = require('./sca-fn-signature-getter') // 函数签名
+var propertiesGetter = require('./sca-properties-getter') // 组件属性列表
 
-require('acorn-jsx/inject')(acorn)
-require('acorn-es7-plugin')(acorn)
+// 初始化配置acorn
+require('acorn-jsx/inject')(acorn) // 支持 React JSX 语法
+require('acorn-es7-plugin')(acorn) // 支持 ES7 语法
 
 /*
  扫描功能入口
@@ -22,7 +24,6 @@ module.exports = function scan(jsContentText, options) {
     'plugins': { jsx: true }
   }))
 
-  // 获取组件内方法签名
   fnSignatureGetter(ast)
 
   return {
