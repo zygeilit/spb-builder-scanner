4d084cbd1be46877a7cdab0d471796c0538f7219
zhangyue <zhangyue@beisen.com>
2018-01-23 14:31:16 +0800

    props getter

--- src/sca-properties-getter.js
+++ src/sca-properties-getter.js
@@ -1,4 +1,20 @@
 
+var walk = require("acorn/dist/walk")
+
 module.exports = function (ast) {
-  
+  walk.findNodeAt(ast, null, null,
+    function (nodeType, node) {
+      // find: let|const|var <cmp-name> = React.createClass({ ... })
+      if (nodeType === 'CallExpression') {
+        let { type, object, property } = node.callee
+        // React.createClass
+        if (type === 'MemberExpression' && object.name === 'React' && property.name === 'createClass') {
+          let [{ properties }] = node.arguments
+          for (var i = 0; i < properties.length; i++) {
+            console.log(properties[i].key.name)
+          }
+        }
+      }
+    }
+  )
 }
